9854e85b7ebe9990480f68f3741fee03a8a0955e
Swarup-Mishra <mridulmishra19@gmail.com>
2019-11-29 14:59:58 +0530

    11282019_30 application

--- appian/ddl_files/ASD_CreateEmployeeTable.sql
+++ appian/ddl_files/ASD_CreateEmployeeTable.sql
@@ -2,12 +2,12 @@ USE `SecondaryDB`;
 
 Start Transaction;
 
-
     drop table if exists `asdemployee`;
 
     create table `asdemployee` (
         `id` integer not null auto_increment,
         `firstname` varchar(255),
+        `middlename` varchar(255),
         `lastname` varchar(255),
         primary key (`id`)
     ) ;
