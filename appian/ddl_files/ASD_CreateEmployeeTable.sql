486487f36d516e156026b9426d8afb2e9dcd37f8
Swarup-Mishra <mridulmishra19@gmail.com>
2019-11-29 14:36:25 +0530

    11282019_29 application

--- appian/ddl_files/ASD_CreateEmployeeTable.sql
+++ appian/ddl_files/ASD_CreateEmployeeTable.sql
@@ -1,15 +1,8 @@
-/*
-Data Store: ASD_DS
-Created: 2019-11-28T07:27:19.917Z
-By: root
-Appian Version: 19.4.43.0
-Target Database: MySQL 5.7.18
-Database Driver: MySQL Connector Java mysql-connector-java-5.1.47 ( Revision: fe1903b1ecb4a96a917f7ed3190d80c049b1de29 )
-*/
+USE `SecondaryDB`;
 
-/* UPDATE DDL */
-/* DROP AND CREATE DDL */
-/*WARNING: The DDL commented out below will drop and re-create all tables.*/
+Start Transaction;
+
+
     drop table if exists `asdemployee`;
 
     create table `asdemployee` (
@@ -18,4 +11,6 @@ Database Driver: MySQL Connector Java mysql-connector-java-5.1.47 ( Revision: fe
         `lastname` varchar(255),
         primary key (`id`)
     ) ;
+
+commit;
 
